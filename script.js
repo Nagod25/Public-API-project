cfad6435478f64424761a52f5e2fd7285c82a754
Joseph Olusegun <70368958+Nagod25@users.noreply.github.com>
2020-12-29 12:58:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("createBtn").innerHTML =
+        document.getElementById("content").innerHTML =
         this.responseText;
       }
     };
